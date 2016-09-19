00ca7da0bd442fe0ada904d1f50eff87577a8272
Xin Tang <xt483@compute.cusp.nyu.edu>
2016-09-19 00:50:37 -0400

    all

---
+++ show_bus_locations_xt483.py
@@ -0,0 +1,28 @@
+import pylab as pl
+import json
+import requests
+import urllib as ulr
+import numpy as np
+
+import os
+%pylab inline
+
+
+import sys
+if __name__ =="main":
+    key = sys.argv[1]
+    busline = sys.argv[2]
+
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + "&VehicleMonitoringDetailLevel=calls&LineRef=" + busline
+
+response = ulr.urlopen(url)
+data = response.read().decode("utf-8")
+data = json.loads(data)
+
+print "Bus Line :", busline
+bustotal = np.size(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+print "Number of Active Buses :",bustotal
+for i in range(bustotal):
+    lat = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    long = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    print "Bus",i+1, "is at latitude", lat,"and longitude",long
